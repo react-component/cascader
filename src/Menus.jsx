a508c127363dbafa2d21dfc883fdb9beff8fc55c
afc163 <afc163@gmail.com>
2015-12-21 18:24:27 +0800

    Add active item

--- src/Menus.jsx
+++ src/Menus.jsx
@@ -14,17 +14,21 @@ class Menus extends React.Component {
     let activeOptions = this.state.activeOptions;
     activeOptions = activeOptions.slice(0, menuIndex + 1);
     activeOptions[menuIndex] = targetOption;
+    this.setState({ activeOptions });
     if (!targetOption.options || targetOption.options.length === 0) {
       this.props.onSelect(activeOptions);
-    } else {
-      this.setState({ activeOptions });
     }
   }
   getShowOptions() {
     let { options } = this.props;
-    const result = this.state.activeOptions.map(activeOption => activeOption.options);
+    const result = this.state.activeOptions
+      .map(activeOption => activeOption.options)
+      .filter(activeOption => !!activeOption);
     result.unshift(options);
     return result;
+  }
+  isActiveOption(option) {
+    return this.state.activeOptions.some(o => o === option);
   }
   render() {
     const { prefixCls } = this.props;
@@ -32,11 +36,17 @@ class Menus extends React.Component {
       <div>
         {this.getShowOptions().map((options, menuIndex) =>
           <ul className={`${prefixCls}-menu`} key={menuIndex}>
-            {options.map(option => (
-              <li key={option.value} onClick={this.handleClick.bind(this, option, menuIndex)}>
+            {options.map(option => {
+              let menuItemCls = `${prefixCls}-menu-item`;
+              if (this.isActiveOption(option)) {
+                menuItemCls += ` ${prefixCls}-menu-item-active`;
+              }
+              return <li key={option.value}
+                  className={menuItemCls}
+                  onClick={this.handleClick.bind(this, option, menuIndex)}>
                 {option.label}
-              </li>
-            ))}
+              </li>;
+            })}
           </ul>
         )}
       </div>
