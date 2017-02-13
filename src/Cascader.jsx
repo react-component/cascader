2049b557b70e3b04d2408829f7680fb3061a0be3
afc163 <afc163@gmail.com>
2017-02-13 11:49:17 +0800

    remove e.preventDefault

--- src/Cascader.jsx
+++ src/Cascader.jsx
@@ -153,10 +153,7 @@ class Cascader extends Component {
     }
     this.setState(newState);
   }
-  handleKeyDown = (e) => {
-    if (e && e.preventDefault) {
-      e.preventDefault();
-    }
+  handleKeyDown = (e) => {  
     const activeValue = [...this.state.activeValue];
     const currentLevel = activeValue.length - 1 < 0 ? 0 : activeValue.length - 1;
     const currentOptions = this.getCurrentLevelOptions();
