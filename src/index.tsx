b9d02b8ed862dcae1f5abcfb1226ef6de0a0f8cb
zombiej <smith3816@gmail.com>
2021-08-27 13:59:04 +0800

    chore: Export interface

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
-import Cascader from './Cascader'
+import Cascader, { CascaderProps } from './Cascader';
+import type { FieldNames, DataNode, ShowSearchType } from './interface';
+
+export { CascaderProps, FieldNames, DataNode, ShowSearchType };
 
 export default Cascader;
