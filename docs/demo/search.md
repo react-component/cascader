89b4ffa395657a0aecd1e76d70b0d5fd6ceb34cb
kiner-tang(文辉) <1127031143@qq.com>
2023-02-20 14:18:33 +0800

    feat: add demo

---
+++ docs/demo/search.md
@@ -0,0 +1,3 @@
+## search
+
+<code src="../../examples/search.jsx">
---
+++ examples/search.jsx
@@ -0,0 +1,75 @@
+/* eslint-disable no-console */
+import React from 'react';
+import '../assets/index.less';
+import Cascader from '../src';
+
+const addressOptions = [
+  {
+    label: '福建',
+    value: 'fj',
+    children: [
+      {
+        label: '福州',
+        value: 'fuzhou',
+        children: [
+          {
+            label: '马尾',
+            value: 'mawei',
+          },
+        ],
+      },
+      {
+        label: '泉州',
+        value: 'quanzhou',
+      },
+    ],
+  },
+  {
+    label: '浙江',
+    value: 'zj',
+    children: [
+      {
+        label: '杭州',
+        value: 'hangzhou',
+        children: [
+          {
+            label: '余杭',
+            value: 'yuhang',
+          },
+          {
+            label: '福州',
+            value: 'fuzhou',
+            children: [
+              {
+                label: '马尾',
+                value: 'mawei',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: '北京',
+    value: 'bj',
+    children: [
+      {
+        label: '朝阳区',
+        value: 'chaoyang',
+      },
+      {
+        label: '海淀区',
+        value: 'haidian',
+      },
+    ],
+  },
+];
+
+class Demo extends React.Component {
+  render() {
+    return <Cascader options={addressOptions} showSearch />;
+  }
+}
+
+export default Demo;
