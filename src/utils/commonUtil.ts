4c751207b38ed3c8b347ad634ab90c4d18cfde83
yuexing <yuexingphp@163.com>
2023-08-24 18:36:59 +0800

    fix(cascader): 🐞 default no go top
    Fix cascading default not returning to top

--- src/utils/commonUtil.ts
+++ src/utils/commonUtil.ts
@@ -43,7 +43,7 @@ export function isLeaf(option: DefaultOptionType, fieldNames: FieldNames) {
   return option.isLeaf ?? !option[fieldNames.children]?.length;
 }
 
-export function scrollIntoParentView(element: HTMLElement) {
+export function scrollIntoParentView(element: HTMLElement, activeValueCells: React.Key[]) {
   const parent = element.parentElement;
   if (!parent) {
     return;
@@ -54,6 +54,12 @@ export function scrollIntoParentView(element: HTMLElement) {
     parent.scrollTo({ top: elementToParent });
   } else if (elementToParent + element.offsetHeight - parent.scrollTop > parent.offsetHeight) {
     parent.scrollTo({ top: elementToParent + element.offsetHeight - parent.offsetHeight });
+  } else {
+    const parentToContainer = parent.parentElement.children;
+    if(activeValueCells.length === parentToContainer.length) return
+    if(typeof parentToContainer[activeValueCells.length] !== undefined){
+      parentToContainer[activeValueCells.length].scrollTo(0, 0)
+    }
   }
 }
 
