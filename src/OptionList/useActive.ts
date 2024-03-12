23767d95e117d01c6ec4b2372965a4d4247946a2
Wanpan <wanpan96@163.com>
2024-03-12 12:51:58 +0800

    fix: optimized reset activeValueCells logic

--- src/OptionList/useActive.ts
+++ src/OptionList/useActive.ts
@@ -11,19 +11,26 @@ export default (
   const { values } = React.useContext(CascaderContext);
 
   const firstValueCells = values[0];
+  const lastValuesRef = React.useRef(null);
 
   // Record current dropdown active options
   // This also control the open status
   const [activeValueCells, setActiveValueCells] = React.useState<React.Key[]>([]);
 
-  // use useLayoutEffect Prevent flickering
+  // use useLayoutEffect timely update of DOM calculation position and Prevent flickering
   React.useLayoutEffect(() => {
     if (open && !multiple) {
-      setActiveValueCells(firstValueCells || []);
-      // no values run reset activeValueCells
-    } else if (values.length === 0) {
-      setActiveValueCells([]);
+      // firstValueCells clear use delay update
+      if (lastValuesRef.current && !firstValueCells) {
+        setTimeout(() => {
+          setActiveValueCells([]);
+        }, 0);
+      } else {
+        setActiveValueCells(firstValueCells || []);
+      }
     }
+
+    lastValuesRef.current = firstValueCells;
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [open, firstValueCells]);
