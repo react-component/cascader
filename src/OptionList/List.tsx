fdcadbb327049ae0102ca0b5646224b677356f62
lihongxiang <lihongxiangbj@kanyun.com>
2024-07-11 10:25:44 +0800

    fix: remove redundance dependency

--- src/OptionList/List.tsx
+++ src/OptionList/List.tsx
@@ -103,7 +103,7 @@ const RawOptionList = React.forwardRef<RefOptionListProps, RawOptionListProps>((
     if (defaultActiveValueCells && defaultActiveValueCells?.length > 0) {
         setActiveValueCells(defaultActiveValueCells)
     }
-  }, [defaultActiveValueCells, setActiveValueCells]);
+  }, [defaultActiveValueCells]);
 
   const isSelectable = (option: DefaultOptionType) => {
     const { disabled } = option;
