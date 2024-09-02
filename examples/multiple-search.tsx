62bbe3c3bf6e7daa97e957eb89ec966b7adfa9d6
叶枫 <7971419+crazyair@users.noreply.github.com>
2024-09-02 17:58:43 +0800

    feat: optimize search (#530)
    * feat: optimize search
    
    * feat: test
    
    * feat: test
    
    * feat: test
    
    * feat: test
    
    * feat: add test

---
+++ examples/multiple-search.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import '../assets/index.less';
+import Cascader from '../src';
+
+const options = [
+  {
+    value: 'zhejiang',
+    label: 'Zhejiang',
+    children: [
+      {
+        value: 'hangzhou',
+        label: 'Hangzhou',
+        children: [
+          {
+            value: 'xihu',
+            label: 'West Lake',
+          },
+          {
+            value: 'xiasha',
+            label: 'Xia Sha',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: 'jiangsu',
+    label: 'Jiangsu',
+    children: [
+      {
+        value: 'nanjing',
+        label: 'Nanjing',
+        children: [
+          {
+            value: 'zhonghuamen',
+            label: 'Zhong Hua men',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const Demo = () => {
+  return <Cascader checkable showSearch options={options} />;
+};
+
+export default Demo;
