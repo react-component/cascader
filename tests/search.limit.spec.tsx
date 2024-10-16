4c13a898e11973219693f5b1c9252a560896de8b
dongbanban <215387880@qq.com>
2024-10-16 15:34:37 +0800

    fix: antd-issue-51248 (#541)
    * fix: antd-issue-51248
    
    * feat: add test for 51248
    
    * feat: update test for limit
    
    * feat: update test for limit
    
    * feat: update test for limit
    
    * feat: update test for limit

---
+++ tests/search.limit.spec.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import Cascader from '../src';
+import type { ReactWrapper } from './enzyme';
+import { mount } from './enzyme';
+
+describe('Cascader.Search', () => {
+  function doSearch(wrapper: ReactWrapper, search: string) {
+    wrapper.find('input').simulate('change', {
+      target: {
+        value: search,
+      },
+    });
+  }
+  const options = [
+    {
+      children: [] as any[],
+      isParent: true,
+      label: 'Asia',
+      value: 'Asia',
+    },
+  ];
+  for (let i = 0; i < 100; i++) {
+    options[0].children.push({
+      label: 'label' + i,
+      value: 'value' + i,
+    });
+  }
+
+  it('limit', () => {
+    const wrapper = mount(
+      <Cascader
+        options={options}
+        open
+        showSearch={{
+          limit: false,
+        }}
+      />,
+    );
+
+    doSearch(wrapper, 'as');
+    const itemList = wrapper.find('div.rc-cascader-menu-item-content');
+    expect(itemList).toHaveLength(100);
+  });
+
+  it('limit', () => {
+    const wrapper = mount(
+      <Cascader
+        options={options}
+        open
+        showSearch={{
+          limit: 0,
+        }}
+      />,
+    );
+
+    doSearch(wrapper, 'as');
+    const itemList = wrapper.find('div.rc-cascader-menu-item-content');
+    expect(itemList).toHaveLength(100);
+  });
+
+  it('limit', () => {
+    const wrapper = mount(
+      <Cascader
+        options={options}
+        open
+        showSearch={{
+          limit: 20,
+        }}
+      />,
+    );
+
+    doSearch(wrapper, 'as');
+    const itemList = wrapper.find('div.rc-cascader-menu-item-content');
+    expect(itemList).toHaveLength(20);
+  });
+});
