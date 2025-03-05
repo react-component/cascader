39a61a8ab2246eede5346ed1d7f5cf0baf36d8c9
小豪 <1844749591@qq.com>
2025-03-05 21:15:09 +0800

    revert

--- src/utils/treeUtil.ts
+++ src/utils/treeUtil.ts
@@ -16,7 +16,7 @@ export function formatStrategyValues(
   const keyPathEntities = getKeyPathEntities();
 
   return pathKeys.filter(key => {
-    const entity = keyPathEntities[key as string];
+    const entity = keyPathEntities[key];
     const parent = entity ? entity.parent : null;
     const children = entity ? entity.children : null;
 
